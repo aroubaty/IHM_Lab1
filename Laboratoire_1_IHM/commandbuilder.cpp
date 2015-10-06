d86cf080e8b285851cd5f422ed5799b04be8694a
Raphaël Racine <raphaelracine@users.noreply.github.com>
2015-10-06 13:08:30 +0200

    Un commit

--- Laboratoire_1_IHM/commandbuilder.cpp
+++ Laboratoire_1_IHM/commandbuilder.cpp
@@ -5,9 +5,9 @@ CommandBuilder::CommandBuilder(QObject *parent) : QObject(parent)
 }
 
 QString CommandBuilder::getCommandLine(QString input, QString output, int start, int time){
-
+    return "ffmpeg -i " + input + " -ss "+start+" -t "+time+" " + output;
 }
 
 QString CommandBuilder::getMeta(QString file){
-
+    return "ffprobe " + file;
 }
